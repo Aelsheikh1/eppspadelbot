945ff2ca3ad7fd059d0400b5eef1c1f5cfcb74dd
Aelsheikh1 <aelsheikh1987@gmail.com>
2025-05-06 12:13:25 +0300

    fix: Initialize currentToken variable in service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-restricted-globals */
 
 // This service worker can be customized!
+let currentToken = null;
 // See https://developers.google.com/web/tools/workbox/modules
 // for the list of available Workbox modules, or add any other
 // code you'd like.
